1609b92bf32105d82d805406e9e64065d3503891
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2022-04-17 11:19:47 +0300

    edd pokemon description and map on pokemon page

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=100)
+    en_name = models.CharField(max_length=100)
+    jp_name = models.CharField(max_length=100)
+    description = models.TextField()
     image = models.ImageField(null=True)
 
     def __str__(self):
