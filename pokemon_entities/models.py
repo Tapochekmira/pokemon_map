45f3ec0c0f2f2c5da57f2b9ed25fb86392201018
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2022-04-20 11:27:37 +0300

    added ignoring unimportant files

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,14 +3,15 @@ from django.db import models
 
 class Pokemon(models.Model):
     name = models.CharField('Название', max_length=100)
-    en_name = models.CharField('Название на английском', max_length=100)
-    jp_name = models.CharField('Название на японском', max_length=100)
-    description = models.TextField('Описание')
+    en_name = models.CharField('Название на английском', max_length=100, blank=True)
+    jp_name = models.CharField('Название на японском', max_length=100, blank=True)
+    description = models.TextField('Описание', blank=True)
     previous_evolution = models.ForeignKey(
         'self',
         verbose_name='Эволюционировал из',
         on_delete=models.SET_NULL,
         null=True,
+        blank=True,
         related_name='next_evolution',
     )
     image = models.ImageField('Изображение', null=True)
@@ -22,14 +23,14 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     latitude = models.FloatField('Долгота')
     longitude = models.FloatField('Широта')
-    appeared_at = models.DateTimeField('Появился в')
-    disappeared_at = models.DateTimeField('Исчез в')
-    level = models.IntegerField('Уровень', default=1)
-    health = models.IntegerField('Жизни', default=100)
-    strength = models.IntegerField('Сила', default=1)
-    defence = models.IntegerField('Защита', default=1)
-    stamina = models.IntegerField('Выносливость', default=1)
-    pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.PROTECT)
+    appeared_at = models.DateTimeField('Появился в', null=True, blank=True)
+    disappeared_at = models.DateTimeField('Исчез в', null=True, blank=True)
+    level = models.IntegerField('Уровень', default=1, null=True, blank=True)
+    health = models.IntegerField('Жизни', default=100, null=True, blank=True)
+    strength = models.IntegerField('Сила', default=1, null=True, blank=True)
+    defence = models.IntegerField('Защита', default=1, null=True, blank=True)
+    stamina = models.IntegerField('Выносливость', default=1, null=True, blank=True)
+    pokemon = models.ForeignKey(Pokemon, verbose_name='Покемон', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.pokemon.name} по координатам ' \
